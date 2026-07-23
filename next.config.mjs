55120e6f72c659aa28cabf3d066a306ce0aa7a45
macici <leonardo.macici@hotmail.com>
2026-07-23 13:37:42 -0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    // Konva tries to require the optional native "canvas" package on the
-    // server. We only render Konva on the client, so stub it out.
-    config.externals = [...config.externals, { canvas: "canvas" }];
-    return config;
-  },
 };
 
 export default nextConfig;
